ba13a5dbcedcce7eea80f769947326f5541ea0eb
Franco Stefanini <mac@MacBook-Pro-de-mac.local>
2022-10-26 21:40:00 -0300

    Agrego los cambios de index.js

--- index.js
+++ index.js
@@ -25,6 +25,27 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listaEl = document.querySelector(".lista")
+
+  const itemsEl = document.querySelectorAll(".lista li")
+  // console.log(itemsEl)
+
+  for (let el of itemsEl) {
+    el.remove()
+  }
+
+
+  for (const item of cosasQueAprendimos) {
+    const newElLi = document.createElement("li")
+    newElLi.textContent = item.tema
+    const clasesQueAgregare = item.class || "item"
+    newElLi.classList.add(clasesQueAgregare)
+    console.log(newElLi)
+    listaEl.appendChild(newElLi)
+  }
+
+
+}
 
 main();
